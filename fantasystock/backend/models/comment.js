fb5912c81a1046b3a743c8d3e5ceb2afd5e63af6
JonahLozano <111929937+JonahLozano@users.noreply.github.com>
2022-09-23 16:53:16 -0700

    Update comment.js

--- fantasystock/backend/models/comment.js
+++ fantasystock/backend/models/comment.js
@@ -1,28 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const findorCreate = require("mongoose-findorcreate");
 
-const User = require('./user');     // requiring user.js to be able to reference the user who is commenting
+const User = require("./user"); // requiring user.js to be able to reference the user who is commenting
 
-const CommentSchema = new Schema({
-    content:{
-        type: String,
-        required: true
+const CommentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
     },
-    entryDate:{
-        type: Date,
-        required: true,
-        default: Date.now
+    user: {
+      type: Schema.Types.ObjectId, // referencing the user for the comment
+      ref: "User",
     },
-    user:{
-        type: Schema.Types.ObjectId,    // referencing the user for the comment
-        ref: 'User'
-    }
-});
-
-//const Comment = mongoose.model("Comment", CommentSchema);
-
-//const mySchema = {'Users': User, 'Comments': Comment}
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    replies: {
+      type: [Schema.Types.ObjectId],
+      ref: this,
+    },
+  },
+  { timestamps: true }
+);
 
 CommentSchema.plugin(findorCreate);
 
