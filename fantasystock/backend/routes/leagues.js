d209bac3c1c0d16a88ddb7b52223ee261a483623
JonahLozano <111929937+JonahLozano@users.noreply.github.com>
2022-10-20 14:09:59 -0700

    Added CRUD for League Comment Section

--- fantasystock/backend/routes/leagues.js
+++ fantasystock/backend/routes/leagues.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 require("dotenv").config();
 const League = require("../models/league");
+const User = require("../models/user");
 
 var jsonParser = bodyParser.json();
 
@@ -52,6 +53,7 @@ router.patch("/join", jsonParser, async (req, res) => {
   // DONT LET USER JOIN IF ALREADY JOINED
   // MAKE SURE USER IS LOGGED IN
   // MAKE SURE START DATE IS AFTER CURRENT DATE
+  // console.log(req.body);
   if (req.body.gameID === undefined || req.body.stocks === undefined) return;
 
   const exists = League.exists({ _id: req.body.gameID });
@@ -85,12 +87,153 @@ router.get("/search", jsonParser, async (req, res) => {
   res.send(ans);
 });
 
+router.patch("/comment", isLoggedIn, jsonParser, async (req, res) => {
+  if (req.body.comment === undefined) return;
+
+  const commentData = {
+    comment: req.body.comment,
+    owner: req.user._id,
+    likes: 0,
+    dislikes: 0,
+    replies: [],
+  };
+
+  const exists = League.exists({ _id: req.body.gameID });
+
+  if (exists) {
+    const game = await League.findById(req.body.gameID);
+
+    console.log(game);
+
+    game.commentsection.push(commentData);
+    game.save();
+  }
+});
+
+router.patch("/comment/edit", jsonParser, async (req, res) => {
+  if (req.body.gameID === undefined || req.body.commentID === undefined) return;
+
+  const exists1 = League.exists({ _id: req.body.gameID });
+
+  if (exists1) {
+    const game = await League.findById(req.body.gameID);
+    const aComment = game.commentsection.map((comment) => {
+      if (comment._id.toString() === req.body.commentID) {
+        const aCopy = comment;
+        aCopy.comment = req.body.comment;
+        return aCopy;
+      } else {
+        return comment;
+      }
+    });
+    game.commentsection = aComment;
+    game.save();
+  }
+});
+
+router.patch("/comment/delete", isLoggedIn, jsonParser, async (req, res) => {
+  console.log(req.body);
+  if (req.body.gameID === undefined || req.body.commentID === undefined) return;
+
+  const exists1 = League.exists({ _id: req.body.gameID });
+
+  if (exists1) {
+    const game = await League.findById(req.body.gameID);
+    const aComment = game.commentsection.filter((comment) => {
+      return comment._id.toString() !== req.body.commentID;
+    });
+    game.commentsection = aComment;
+    game.save();
+  }
+});
+
+router.patch("/comment/reply", isLoggedIn, jsonParser, async (req, res) => {
+  // console.log(req.body);
+  if (req.body.comment === undefined) return;
+
+  const commentData = {
+    reply: req.body.comment,
+    replyowner: req.user._id,
+    likes: 0,
+    dislikes: 0,
+  };
+
+  const exists = League.exists({ _id: req.body.gameID });
+
+  if (exists) {
+    const game = await League.findById(req.body.gameID);
+
+    // console.log(game);
+
+    game.commentsection.forEach(
+      (ele) =>
+        ele._id.toString() === req.body.commentID &&
+        ele.replies.push(commentData)
+    );
+
+    console.log(game);
+    game.save();
+  }
+});
+
 router.get("/:id", async (req, res) => {
   const exists = League.exists({ _id: req.params.id });
 
   if (exists) {
     const game = await League.findById(req.params.id);
-    res.send(game);
+    // console.log(game);
+
+    const host = await User.findById({ _id: game.host });
+
+    const players = await Promise.all(
+      game.players.map(async (data) => {
+        return {
+          player: await User.findById(data.player),
+          stocks: data.stocks,
+        };
+      })
+    );
+
+    const commentCollection = await Promise.all(
+      game.commentsection.map(async (data) => {
+        return {
+          comment: data.comment,
+          owner: await User.findById(data.owner),
+          likes: data.likes,
+          dislikes: data.dislikes,
+          replies: await Promise.all(
+            data.replies.map(async (ele) => {
+              const replyUser = await User.findById(ele.replyowner);
+              console.log(replyUser);
+              return {
+                reply: ele.reply,
+                replyowner: replyUser,
+                replylikes: ele.replylikes,
+                replydislikes: ele.replydislikes,
+                isOwner: ele.replyowner.toString() === req.user._id,
+              };
+            })
+          ),
+          isOwner: req.user._id === data.owner.toString(),
+          commentID: data._id,
+        };
+      })
+    );
+
+    const aGame = {
+      title: game.title,
+      host,
+      visibility: game.visibility,
+      start: game.start,
+      end: game.end,
+      players,
+      commentsection: commentCollection,
+    };
+
+    // console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    // console.log(aGame.commentsection);
+
+    res.send(aGame);
   }
 });
 
