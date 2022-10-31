76947a1e233b13b5d585164bce4c170557b47638
JonahLozano <111929937+JonahLozano@users.noreply.github.com>
2022-10-30 23:43:35 -0700

    Added League Center

--- fantasystock/frontend/src/containers/LeagueCenter/LeagueCenter.jsx
+++ fantasystock/frontend/src/containers/LeagueCenter/LeagueCenter.jsx
@@ -1,19 +1,46 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function LeagueCenter() {
-  //   const [articles, setArticles] = useState([]);
+  const [leagues, setLeagues] = useState({});
+  const [show, setShow] = useState(false);
 
-  //   useEffect((event) => {
-  //     axios
-  //       .get(`/news`)
-  //       .then((response) => {
-  //         setArticles(response.data);
-  //       })
-  //       .catch((error) => {});
-  //   }, []);
+  useEffect((event) => {
+    axios
+      .get(`/register/view/leagues`)
+      .then((response) => {
+        console.log(response.data);
+        setLeagues(response.data);
+        setShow(true);
+      })
+      .catch((error) => {
+        setShow(false);
+      });
+  }, []);
 
-  return <div>Leagues Center</div>;
+  return (
+    <div>
+      <h1>Active Leagues</h1>
+      {show &&
+        leagues.activeLeagues.map((league, index) => (
+          <div key={index}>
+            <Link className="searchResult" to={`/league/${league._id}`}>
+              <div className="searchResultTitle">{league.title}</div>
+            </Link>
+          </div>
+        ))}
+      <h1>Passed Leagues</h1>
+      {show &&
+        leagues.passedLeagues.map((league, index) => (
+          <div key={index}>
+            <Link className="searchResult" to={`/league/${league._id}`}>
+              <div className="searchResultTitle">{league.title}</div>
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default LeagueCenter;
