01980253328e9a39e5a5b752870c40b3bbda6291
JonahLozano <111929937+JonahLozano@users.noreply.github.com>
2022-10-30 23:52:08 -0700

    Nothing Changed Just Auto Formated

--- fantasystock/frontend/src/containers/CreateLeague/CreateLeague.jsx
+++ fantasystock/frontend/src/containers/CreateLeague/CreateLeague.jsx
@@ -16,19 +16,23 @@ function Home() {
   const [tickers, setTickers] = useState([]);
 
   const createLeague = () => {
-    axios.post("/league/create", {
-      start,
-      end,
-      title,
-      visibility: visibility.toLowerCase(),
-      stocks: stkList.map((aStock) => {
-        return {
-          stock: aStock.stock,
-          quantity: aStock.quantity,
-          position: aStock.position.toLowerCase(),
-        };
-      }),
-    });
+    axios
+      .post("/league/create", {
+        start,
+        end,
+        title,
+        visibility: visibility.toLowerCase(),
+        stocks: stkList.map((aStock) => {
+          return {
+            stock: aStock.stock,
+            quantity: aStock.quantity,
+            position: aStock.position.toLowerCase(),
+          };
+        }),
+      })
+      .then((e) => {
+        console.log(e.data);
+      });
   };
 
   useEffect((event) => {
@@ -62,133 +66,117 @@ function Home() {
 
   return (
     <div id="CreateLeague">
-    <div id="CL">
-      <div id="CLTitle">
-        <label htmlFor="clTitle">
-          Title
-        </label>
-        <input
-          id="clTitle"
-          type="textbox"
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-          value={title}
-        />
+      <div id="CL">
+        <div id="CLTitle">
+          <label htmlFor="clTitle">Title</label>
+          <input
+            id="clTitle"
+            type="textbox"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            value={title}
+          />
+        </div>
+        <div id="CLVisibility">
+          <label htmlFor="visibility">League</label>
+          <select
+            id="visibility"
+            onChange={(e) => setVisibility(e.target.value)}
+          >
+            <option>Public</option>
+            <option>Private</option>
+          </select>
+        </div>
+        <div id="CLStart">
+          <label htmlFor="start">Start</label>
+          <input
+            id="start"
+            type="date"
+            onChange={(e) => {
+              setStart(e.target.value);
+            }}
+            value={start}
+          />
+        </div>
+        <div id="CLEnd">
+          <label htmlFor="end">End</label>
+          <input
+            id="end"
+            type="date"
+            onChange={(e) => {
+              setEnd(e.target.value);
+            }}
+            value={end}
+          />
+        </div>
       </div>
-      <div id="CLVisibility">
-        <label
-          htmlFor="visibility"
-        >
-          League
-        </label>
-        <select id="visibility" onChange={(e) => setVisibility(e.target.value)}>
-          <option>Public</option>
-          <option>Private</option>
-        </select>
-      </div>
-      <div id="CLStart">
-        <label htmlFor="start">
-          Start
-        </label>
-        <input
-          id="start"
-          type="date"
-          onChange={(e) => {
-            setStart(e.target.value);
-          }}
-          value={start}
-        />
-      </div>
-      <div id="CLEnd">
-        <label htmlFor="end">
-          End
-        </label>
-        <input
-          id="end"
-          type="date"
-          onChange={(e) => {
-            setEnd(e.target.value);
-          }}
-          value={end}
-        />
-      </div>
-    </div>
       <div id="LeagueJoinStocks">
         <h4>Stocks</h4>
-      <div>
-        <div id="StockOptSelect">
-          <label htmlFor="clTicker">
-            Ticker:
-          </label>
-          <input
-            id="clTicker"
-            type="text"
-            list="data"
-            onChange={(e) => {
-              setStk(e.target.value);
-            }}
-            value={stk}
-            placeholder="Pick a stock"
-          />
-          <datalist id="data">
-            {tickers.map((item, key) => (
-              <option key={key} value={item} />
-            ))}
-          </datalist>
+        <div>
+          <div id="StockOptSelect">
+            <label htmlFor="clTicker">Ticker:</label>
+            <input
+              id="clTicker"
+              type="text"
+              list="data"
+              onChange={(e) => {
+                setStk(e.target.value);
+              }}
+              value={stk}
+              placeholder="Pick a stock"
+            />
+            <datalist id="data">
+              {tickers.map((item, key) => (
+                <option key={key} value={item} />
+              ))}
+            </datalist>
 
-          <label htmlFor="clQuantity">
-            Quantity:
-          </label>
-          <input
-            id="clQuantity"
-            type="number"
-            min="1"
-            max="999"
-            onChange={(e) => {
-              setQnt(e.target.value);
-            }}
-            value={qnt}
-          />
+            <label htmlFor="clQuantity">Quantity:</label>
+            <input
+              id="clQuantity"
+              type="number"
+              min="1"
+              max="999"
+              onChange={(e) => {
+                setQnt(e.target.value);
+              }}
+              value={qnt}
+            />
 
-          <label htmlFor="clPosition">
-            Position:
-          </label>
-          <select
-            id="clPosition"
-            onChange={(e) => setPos(e.target.value)}
-            value={pos}
-          >
-            <option>Long</option>
-            <option>Short</option>
-          </select>
+            <label htmlFor="clPosition">Position:</label>
+            <select
+              id="clPosition"
+              onChange={(e) => setPos(e.target.value)}
+              value={pos}
+            >
+              <option>Long</option>
+              <option>Short</option>
+            </select>
 
-          <input
-            type="button"
-            value="Pick"
-
-            onClick={stash}
-          />
-        </div>
-        </div>
-          <div id="StockOptList">
-            <h4> My Stocks </h4>
-            {stkList.map((aStock, index) => (
-              <div className="MyStockOpt" key={index}>
-                <p>{aStock.stock} {aStock.quantity} {aStock.position}</p>
-                <input
-                  type="button"
-                  value="delete"
-                  onClick={() => deleteStk(index)}
-                />
-              </div>
-            ))}
+            <input type="button" value="Pick" onClick={stash} />
           </div>
         </div>
+        <div id="StockOptList">
+          <h4> My Stocks </h4>
+          {stkList.map((aStock, index) => (
+            <div className="MyStockOpt" key={index}>
+              <p>
+                {aStock.stock} {aStock.quantity} {aStock.position}
+              </p>
+              <input
+                type="button"
+                value="delete"
+                onClick={() => deleteStk(index)}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
 
       <div id="CLcreate">
-          <input type="button" value="Create League" onClick={createLeague} />
-        </div>
+        <input type="button" value="Create League" onClick={createLeague} />
+      </div>
     </div>
   );
 }
