c3a0c5a5d9d16d7d00c405172937bd6e5b98acee
JonahLozano <111929937+JonahLozano@users.noreply.github.com>
2022-10-30 23:46:05 -0700

    Added Game Logic

--- fantasystock/backend/utils/updateLeagues.js
+++ fantasystock/backend/utils/updateLeagues.js
@@ -1,15 +1,74 @@
-// QUICK PSEUDOCODE TEMPLATE
-// --------------------------
-// EXPORT MODULE
-// ALL_LEAGUES = LEAGUE.FIND_ALL($LTE TODAY)
-// SORT_LEAGUES = ALL_LEAGUES.MAP(LEAGUE => LEAGUE.PLAYER_LIST.MAP(PLAYER => { _id: PLAYER._id, GROWTH: (PLAYER.END_MONEY - PLAYER.START_MONEY) / PLAYER.START_MONEY }).SORT((PLAYER1,PLAYER2) => PLAYER1.GROWTH < PLAYER2.GROWTH ? -1 : PLAYER1.GROWTH > PLAYER2.GROWTH ? 1 : 0 ))
-// SORTED_LEAGUES.FOREACH(LEAGUE => LEAGUE.FOREACH((PLAYER,INDEX) => PLAYER._id.CURRENCY += 10000 * LEAGUE.AMOUNT_OF_PLAYERS * LEAGUE.DURATION_IN_DAYS * (1 / INDEX) ))
-//
-//
-//
-// SIMPLIFIED
-// -----------
-// 0. EXPORT MODULE
-// 1. FIND ALL LEAGUES THAT HAVE AN END DATE BEFORE CURRENT DATE (SEARCH LEAGUE DATABASE)
-// 2. RANK ALL PLAYERS BASED ON GROWTH
-// 3. DISTRIBUTE CURRENCY BASED ON RANK (FIND AND UPDATE USER DATABASE)
+const League = require("../models/league");
+const User = require("../models/user");
+const Stock = require("../models/stock");
+const user = require("../models/user");
+
+module.exports = async () => {
+  const right_now = new Date();
+
+  const updatable_leagues = await League.find({
+    start: { $lte: right_now },
+    active: true,
+  });
+
+  const all_stocks = await Stock.find({});
+
+  updatable_leagues.forEach((league) => {
+    const players_growth = league.players.map((player) => {
+      const newPortfolio = player.stocks.reduce((prev, acc) => {
+        const priceAtNow = all_stocks.find(
+          (ele) => ele.ticker === acc.ticker
+        ).price;
+
+        const ans =
+          acc.position === "long"
+            ? (priceAtNow - acc.priceAtTime) * acc.quantity
+            : acc.position === "short"
+            ? (-priceAtNow + acc.priceAtTime) * acc.quantity
+            : 0;
+
+        return prev + ans;
+      }, 0);
+
+      const oldPortfolio = player.stocks.reduce((prev, acc) => {
+        const ans =
+          acc.position === "long"
+            ? acc.priceAtTime * acc.quantity
+            : acc.position === "short"
+            ? -acc.priceAtTime * acc.quantity
+            : 0;
+        return prev + ans;
+      }, 0);
+
+      return {
+        player: player.player,
+        growth: newPortfolio / oldPortfolio,
+      };
+    });
+
+    const sorted_leagues = players_growth.sort((a, b) => {
+      if (a.growth < b.growth) {
+        return 1;
+      }
+      if (a.growth > b.growth) {
+        return -1;
+      }
+      return 0;
+    });
+
+    console.log(sorted_leagues);
+
+    sorted_leagues.forEach(async (player, index) => {
+      const aUser = await User.findById({ _id: player.player.toString() });
+      aUser.currency += league.players.length * 1000 * (1 / (index + 1));
+      aUser.activeLeagues = aUser.activeLeagues.filter(
+        (league) => league.toString() === league._id
+      );
+      aUser.passedLeagues.push(league._id.toString());
+      aUser.save();
+    });
+
+    league.active = false;
+    league.save();
+  });
+};
