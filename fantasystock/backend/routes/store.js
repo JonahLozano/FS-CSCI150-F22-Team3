5385793cd6157d7d6606983f231373a594469726
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2022-11-07 17:53:12 -0800

    Data Validation for store.js
    Specifics on what data was validated is in comments for '/buy'.

--- fantasystock/backend/routes/store.js
+++ fantasystock/backend/routes/store.js
@@ -9,27 +9,36 @@ var jsonParser = bodyParser.json();
 require("dotenv").config();
 
 router.patch("/buy", jsonParser, async (req, res) => {
-  // CHECK TO SEE IF ALL PARAMETERS ARE HERE
-  // CHECK TO SEE IF ITEM ID IS REAL
-  // FIND ITEM
-  // FIND USER
 
+  // host is the current user
   const host = await User.findById({ _id: req.user._id });
+  // if req.body.item is in inventory.js, itemThing will be equal to ex: { name: 'amazon', price: 1000 }, else undefined
   const itemThing = Inventory.find((ele) => ele.name === req.body.item);
-  const owns = Inventory.includes(itemThing.name);
+  // if req.body.item is in inventory.js, owns will be true, else false
+  const owns = Inventory.some((item) => item.name === req.body.item);
 
-  if (host.currency > itemThing.price && !owns) {
-    console.log("Sufficient Funds");
-    host.currency = host.currency - itemThing.price;
-    host.icons.push(itemThing.name);
-    host.save();
-  } else {
-    console.log("Insufficient Funds");
+  //console.log(host);
+  //console.log(itemThing.name);
+  //console.log(host.icons.includes(itemThing.name));
+
+  // data validation on example: { item: 'amazon' }
+  if(req.body.item === undefined ||         // item name trying to be bought is undefined
+     typeof req.body.item !== "string" ||   // item name trying to be bought is not type string
+     itemThing.name === undefined ||        // item name is not in the inventory (item does not exist)
+     host.currency < itemThing.price ||     // user balance is less than item price
+     !owns ||                               // item is not in the inventory.js
+     host.icons.includes(itemThing.name))   // user already owns the item
+  {
+    console.log("Could not buy item");
+    return;
   }
 
-  // DECREMENT USERS CURRENCY BY AMOUNT FOR ICON
-  // ADD ITEM TO USER ICONS LIST
-  // SAVE USER
+  // passed all error checks, so user can successfully buy item
+  console.log("Sufficient Funds");
+  host.currency = host.currency - itemThing.price;
+  host.icons.push(itemThing.name);
+  host.save();
+  
 });
 
 router.get("/", async (req, res) => {
